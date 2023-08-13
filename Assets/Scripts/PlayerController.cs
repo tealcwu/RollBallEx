799b3829c644dadc71d2c960184ab6f8f5cbad7e
Tealc Wu <tealcwu@hotmail.com>
2023-08-13 15:03:53 +0800

    V0.1: Initial version, same as Unity's tutorial project. * [New] Moving player with Input System * [New] Camera follow with player * [New] Creating and collecting collectibles * [New] Display score and result text

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Player movement speed
+    public float Speed = 10f;
+
+    // Text to show scores
+    public TextMeshProUGUI ScoreText;
+
+    // Text to show game result
+    public GameObject ResultText;
+
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    private float score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0f, movementY);
+        rb.AddForce(movement * Speed);
+    }
+
+    /// <summary>
+    /// Handle user input
+    /// </summary>
+    /// <param name="movementValue"></param>
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    /// <summary>
+    /// Handle trigger enter event
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("PickUp"))
+        {
+            // deactive the pickup collied with player
+            other.gameObject.SetActive(false);
+            score++;
+            SetText();
+        }
+    }
+
+    /// <summary>
+    /// Update UI text
+    /// </summary>
+    private void SetText()
+    {
+        ScoreText.text = "SCORE:" + score.ToString();
+
+        if(score > 5)
+        {
+            ResultText.SetActive(true);
+        }
+    }
+}
---
+++ ReleaseNote.md
@@ -0,0 +1,12 @@
+## **RollBallEx Release Note**
+
+#### V0.1
+
+2023/08/13 14:50
+
+Initial version, same as Unity's tutorial project.
+
+* [New] Moving player with Input System
+* [New] Camera follow with player
+* [New] Creating and collecting collectibles
+* [New] Display score and result text
